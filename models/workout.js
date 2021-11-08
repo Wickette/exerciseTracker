6be95b5cfe00f43c97049e084a70ef1a049bf97f
Wickette <stephany.clark2@gmail.com>
2021-11-08 18:02:06 -0500

    workout model created

---
+++ models/workout.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    name: {
+        type: String
+    },
+    type: {
+        type: String
+    },
+    weight: {
+        type: Number
+    },
+    sets: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    duration: {
+        type: Number
+    },
+    distance: {
+        type: Number
+    },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
